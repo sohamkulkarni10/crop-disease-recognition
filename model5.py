dbe57b4fa72c3ef1816ef4381c5075131a610ad8
sohamkulkarni10 <136838949+sohamkulkarni10@users.noreply.github.com>
2024-12-25 21:36:05 +0530

    Update model5.py

--- model5.py
+++ model5.py
@@ -7,7 +7,7 @@ import os
 
 # Prediction function
 def model_prediction(test_image):
-    model = tf.keras.models.load_model('trained_model.h5')
+    model = tf.keras.models.load_model('trained_model.keras.zip')
     image = tf.keras.preprocessing.image.load_img(test_image, target_size=(128, 128), color_mode="rgb")
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])
